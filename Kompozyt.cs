8be474ee65e440c4d9dab0938df80ac04b9cc40f
AllysBlaze <44466481+AllysBlaze@users.noreply.github.com>
2020-06-30 20:21:50 +0200

    Add project files.

---
+++ Kompozyt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wzorzec_kompozyt
+{
+    abstract class Komponent
+    {
+        protected string Nazwa;
+        public Komponent(string nazwa)
+        {
+            Nazwa = nazwa;
+        }
+        public abstract void Wypisz();
+    }
+    class Kompozyt:Komponent
+    {
+        public Kompozyt(string nazwa) : base(nazwa) { }
+        private List<Komponent> Komponenty = new List<Komponent>();
+        public override void Wypisz()
+        {
+            Console.WriteLine("Kompozyt "+Nazwa+"\n{");
+            foreach (var k in Komponenty)
+                k.Wypisz();
+            Console.WriteLine("}");
+        }
+        public void Dodaj(Komponent komponent)
+        {
+            Komponenty.Add(komponent);
+        }
+    }
+    class Lisc:Komponent
+    {
+        public Lisc(string nazwa) : base(nazwa) { }
+        public override void Wypisz()
+        {
+            Console.WriteLine("lisc " +Nazwa);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wzorzec_kompozyt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lisc l = new Lisc("l");
+            Lisc ll = new Lisc("ll");
+            Lisc l1 = new Lisc("l1");
+            Lisc l2 = new Lisc("l2");
+            Kompozyt k = new Kompozyt("k");
+            Kompozyt k2 = new Kompozyt("k2");
+            Kompozyt k3 = new Kompozyt("k3");
+            k.Dodaj(l);
+            k.Dodaj(k2);
+            k2.Dodaj(k3);
+            k2.Dodaj(ll);
+            k3.Dodaj(l1);
+            k3.Dodaj(l2);
+            k.Wypisz();
+            Console.ReadKey();
+            
+        }
+    }
+}
